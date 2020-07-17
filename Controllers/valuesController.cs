8fc2005c4be7b44933f48a0a2f49b5a419b02d7e
Shivangi Singh <shivangisingh211@outlook.com>
2020-07-17 17:09:57 +0530

    Add project files.

---
+++ Controllers/valuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dockerdemoex.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+    {
+        return new string[] { "Shivangi", "Singh" };
+    }
+}
